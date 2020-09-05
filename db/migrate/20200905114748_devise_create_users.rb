1d86610b345d1ec1ca492f4b7ae479a1e1dbdf66
katimoto <kati.miyamoto@gmail.com>
2020-09-06 00:24:11 +0900

    ユーザー情報

--- db/migrate/20200905114748_devise_create_users.rb
+++ db/migrate/20200905114748_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :f_kana, null: false
+      t.string :l_kana, null: false
+      t.date :date, null: false
 
       ## Recoverable
       t.string   :reset_password_token
