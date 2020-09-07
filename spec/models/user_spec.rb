e828bd3b8ed99fe211b552324dc4143f2faaca6b
katimoto <kati.miyamoto@gmail.com>
2020-09-07 15:02:13 +0900

    修正版

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,7 +57,11 @@ describe User do
         expect(@user.errors.full_messages).to include("Email is invalid")
       end
       it "重複したemailが存在する場合登録できない" do
-
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
       it "passwordが空では登録できない" do
         @user.password = ""
