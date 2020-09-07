7ca32498faae91b3e7199c19358626022bde3d8f
katimoto <kati.miyamoto@gmail.com>
2020-09-07 15:31:16 +0900

    修正版take3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,6 @@ class ItemsController < ApplicationController
   private
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+      redirect_to action: :index unless user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
 
   root to: 'items#index'
   resources :items, only: [:index]
-  resources :users, only: [:new, :create]
 end
