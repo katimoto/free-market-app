507b71086cba1630bd29e8e26369fe2b3688a0f5
katimoto <kati.miyamoto@gmail.com>
2020-09-08 19:17:31 +0900

    改訂版

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    #@items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
