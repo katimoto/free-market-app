92a2119549549c35bdf700d9395d098b18587f2e
katimoto <kati.miyamoto@gmail.com>
2020-09-10 20:02:00 +0900

    修正版

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_index, except: [:index, :show]
 
   def index
@@ -19,11 +19,7 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to item_path
