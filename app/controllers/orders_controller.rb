f4eb38ccf6bead827d0234425b69bc9c1672fff1
katimoto <kati.miyamoto@gmail.com>
2020-09-11 19:01:34 +0900

    ログインチェック

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index
+
   def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
   end
 
   private
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
 
   # def order_params
   #   params.require(:order).permit()
